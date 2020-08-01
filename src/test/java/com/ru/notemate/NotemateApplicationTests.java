95ff2a52bcffd4e95cbc25c671237dcdf1b5385f
ruhat <ruhat@ediz-3.local>
2020-08-01 12:07:58 +0300

    Initial commit

---
+++ src/test/java/com/ru/notemate/NotemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ru.notemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
